53cf8ebea8292542208f86e0ce5f7b8c0fb15e33
Zachary Lentz <zlentz@slac.stanford.edu>
2025-04-09 16:55:48 -0700

    FIX: make sure to set the velocity in the motor test (defaults to zero)

--- tests/test_motors.py
+++ tests/test_motors.py
@@ -8,6 +8,7 @@ from pcdsdevices.epics_motor import BeckhoffAxis
 @pytest.mark.parametrize("suffix", ("M1", "M2"))
 def test_basic_motion(suffix: str):
     mot = BeckhoffAxis(f"PLC:TST:IOC:{suffix}", name=suffix.lower())
+    mot.velocity.put(10)
     goal = mot.position + 4
     mot.move(goal, wait=True)
     assert mot.position == pytest.approx(goal)
