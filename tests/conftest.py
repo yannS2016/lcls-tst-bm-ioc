0bd9bc6a6b03080cabbab73e4c0cbcfd9e863585
Zachary Lentz <zlentz@slac.stanford.edu>
2025-04-09 17:05:09 -0700

    FIX: improper fixture definitions

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,18 +5,18 @@ import pytest
 from .devices import AllPVs
 
 
-@pytest.mark.fixture(scope="session")
+@pytest.fixture(scope="session")
 def all_pvs():
     pvs = AllPVs()
     pvs.wait_for_connection()
     return pvs
 
 
-@pytest.mark.fixture(scope="session")
+@pytest.fixture(scope="session")
 def vars_incr(all_pvs: AllPVs):
     return all_pvs.incr
 
 
-@pytest.mark.fixture(scope="session")
+@pytest.fixture(scope="session")
 def vars_set(all_pvs: AllPVs):
     return all_pvs.setp
