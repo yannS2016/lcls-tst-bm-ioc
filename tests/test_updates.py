3bfef7a577a17e3fa0397bf002e724e56c7897f4
Zachary Lentz <zlentz@slac.stanford.edu>
2025-04-09 17:30:41 -0700

    TST: make the get test more consistent

--- tests/test_updates.py
+++ tests/test_updates.py
@@ -9,8 +9,21 @@ from .devices import PytmcVars
 
 
 def test_basic_gets(vars_incr: PytmcVars):
-    before = vars_incr.get()
-    time.sleep(3)
-    after = vars_incr.get()
-    for key, value in before.items():
-        assert after[key] != pytest.approx(value)
+    # It's not necessarily consistent when the new value is ready
+    # Collect a few samples and make sure at least one pair has a change
+    samples = [vars_incr.get()]
+    for _ in range(5):
+        time.sleep(1)
+        samples.append(vars_incr.get())
+
+    ok_list = []
+    for s1, s2 in zip(samples[:-1], samples[1:]):
+        ok = True
+        for before_val, after_val in zip(s1, s2):
+            if before_val == pytest.approx(after_val):
+                ok = False
+                break
+        ok_list.append(ok)
+
+    assert any(ok_list)
+    assert not all(ok_list)
