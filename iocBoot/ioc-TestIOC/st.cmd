8819e48e31366a59e9439e9d738f9867fab10deb
ysmandza <ysmandza@slac.stanford.edu>
2026-03-09 14:46:45 -0700

    upated boot config

--- iocBoot/ioc-TestIOC/st.cmd
+++ iocBoot/ioc-TestIOC/st.cmd
@@ -6,8 +6,8 @@
 # Generated using: pytmc 2.19.1
 # Project version: unknown
 #    Project hash: unknown
-#     PLC IP/host: 172.21.148.160
-#      PLC Net ID: 172.21.148.160.1.1
+#     PLC IP/host: 172.21.148.154
+#      PLC Net ID: 172.21.148.154.1.1
 # ** DEVELOPMENT MODE IOC **
 # * Using IOC boot directory for autosave.
 # * Archiver settings will not be configured.
@@ -47,8 +47,8 @@ dbLoadDatabase("$(ADS_IOC_TOP)/dbd/adsIoc.dbd")
 adsIoc_registerRecordDeviceDriver(pdbbase)
 
 epicsEnvSet("ASYN_PORT",        "ASYN_PLC")
-epicsEnvSet("IPADDR",           "172.21.148.160")
-epicsEnvSet("AMSID",            "172.21.148.160.1.1")
+epicsEnvSet("IPADDR",           "172.21.148.154")
+epicsEnvSet("AMSID",            "172.21.148.154.1.1")
 epicsEnvSet("AMS_PORT",         "851")
 epicsEnvSet("ADS_MAX_PARAMS",   "27755")
 epicsEnvSet("ADS_SAMPLE_MS",    "50")
@@ -57,7 +57,7 @@ epicsEnvSet("ADS_TIMEOUT_MS",   "1000")
 epicsEnvSet("ADS_TIME_SOURCE",  "0")
 
 # Add a route to the PLC automatically:
-system("${ADS_IOC_TOP}/scripts/add_route.sh 172.21.148.160 ^172.*$")
+system("${ADS_IOC_TOP}/scripts/add_route.sh 172.21.148.154 ^172.*$")
 
 # adsAsynPortDriverConfigure(portName, ipaddr, amsaddr, amsport,
 #    asynParamTableSize, priority, noAutoConnect, defaultSampleTimeMS,
@@ -166,7 +166,7 @@ dbLoadRecords("caPutLog.db", "IOC=$(IOC)")
 dbLoadRecords("TwinCAT_TaskInfo.db", "PORT=$(ASYN_PORT),PREFIX=PLC:TestIOC,IDX=1,TASK_PORT=350")
 dbLoadRecords("TwinCAT_AppInfo.db", "PORT=$(ASYN_PORT), PREFIX=PLC:TestIOC")
 
-dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:TestIOC,PROJECT=TestIOC.tsproj,HASH=unknown,VERSION=unknown,PYTMC=2.19.1,PLC_HOST=172.21.148.160")
+dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:TestIOC,PROJECT=TestIOC.tsproj,HASH=unknown,VERSION=unknown,PYTMC=2.19.1,PLC_HOST=172.21.148.154")
 
 #   LCLS General: * (SLAC)
 dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:TestIOC,DEPENDENCY=LCLS_General,VERSION=*,VENDOR=SLAC")
